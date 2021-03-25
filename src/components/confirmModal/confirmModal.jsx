f0955e2d19b22d586e6c1392b30884b280621795
Lucas Luz <LUCAS_LUZ@jabil.com>
2021-03-25 09:12:39 -0300

    updating css

--- src/components/confirmModal/confirmModal.jsx
+++ src/components/confirmModal/confirmModal.jsx
@@ -3,23 +3,26 @@ import "../../assets/confirmModal/style.css"
 
 class ConfirmModal extends Component{
 
-    _handleCancel(){
-        debugger;
+    _handleCloseModal(){
         const modal = document.querySelectorAll(".confirm-modal")[this.props.id];
         modal.style.display = "none";
         const card = document.querySelectorAll(".card-content")[this.props.id];
         card.style.display = "flex";
     }
 
+    _handleCancel(){
+        this._handleCloseModal();
+    }
+
     _handleConfirm(){
-        this._handleCancel();
+        this._handleCloseModal();
         this.props.handleDelete(this.props.id);
     }
 
     render(){
         return(
             <section className="confirm-modal" data-id={this.props.id}>
-                <button className="close-button-modal" onClick={this._handleCancel.bind(this)}>x</button>
+                <button className="close-button-modal" onClick={this._handleCloseModal.bind(this)}>x</button>
                 <h3 className="title-confirm-modal">Atenção!</h3>
                 <p className="description-confirm-modal">Você tem certeza que deseja excluir esta tarefa?</p>
                 <section className="buttons-confirm-modal">
