f864272f1d3d7132fa32f897c5b9b15994038bc1
Lucas Luz <LUCAS_LUZ@jabil.com>
2021-03-26 14:46:42 -0300

    updating taskcard

--- src/components/taskCard/taskCard.jsx
+++ src/components/taskCard/taskCard.jsx
@@ -1,32 +1,38 @@
 import React, { Component } from "react";
 import ConfirmModal from "../confirmModal/";
 import "../../assets/taskCard/style.css";
-import deleteSVG from "../../assets/img/delete.svg";
+import { ReactComponent as DeleteSVG } from "../../assets/img/delete.svg";
 
 class TaskCard extends Component {
-	    
-    _handleDelete() {
-        const modal = document.querySelectorAll(".confirm-modal")[this.props.id];
-        modal.style.display = "block";
-        const card = document.querySelectorAll(".card-content")[this.props.id];
-        card.style.display = "none";
+	_handleDelete() {
+		const modal = document.querySelectorAll(".confirm-modal")[this.props.id];
+		modal.style.display = "block";
+		const card = document.querySelectorAll(".card-content")[this.props.id];
+		card.style.display = "none";
 	}
-    
-    handleDelete(id){
-        this.props.handleDelete(id);
-    }
+
+	handleDelete(id) {
+		this.props.handleDelete(id);
+	}
 
 	render() {
 		return (
 			<section className="card-task-card">
-                <ConfirmModal id={this.props.id} handleDelete={this.props.handleDelete.bind(this)}></ConfirmModal>
-                <section className="card-content">
-					<img className="task-card-delete" alt="delete" onClick={() => {
-						this._handleDelete(this.props.id)}} src={deleteSVG}
+				<ConfirmModal
+					id={this.props.id}
+					handleDelete={this.props.handleDelete.bind(this)}
+				></ConfirmModal>
+				<section className="card-content">
+					<DeleteSVG
+						className="task-card-delete"
+						onClick={() => {
+							this._handleDelete(this.props.id);
+						}}
+						src={deleteSVG}
 					/>
-				    <h3 className="title-task-card">{this.props.task.title}</h3>
-				    <p className="description-task-card">{this.props.task.description}</p>
-                </section>
+					<h3 className="title-task-card">{this.props.task.title}</h3>
+					<p className="description-task-card">{this.props.task.description}</p>
+				</section>
 			</section>
 		);
 	}
