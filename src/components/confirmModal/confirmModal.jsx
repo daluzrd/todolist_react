69940107db287e40fbfc587f93300cf53657b9cb
Lucas Luz <LUCAS_LUZ@jabil.com>
2021-04-20 09:00:45 -0300

    confirmModal, registerForm and taskCard to funcCom

--- src/components/confirmModal/confirmModal.jsx
+++ src/components/confirmModal/confirmModal.jsx
@@ -1,33 +1,35 @@
-import React, { Component } from "react";
-import "../../assets/confirmModal/style.css"
+import React from "react";
+import "../../assets/confirmModal/style.css";
 
-class ConfirmModal extends Component{
+function ConfirmModal({ handleDelete, id }) {
+	function _handleHideModal() {
+		const modal = document.querySelectorAll(".confirm-modal")[id];
+		modal.style.display = "none";
+		const card = document.querySelectorAll(".card-content")[id];
+		card.style.display = "flex";
+	}
 
-    _handleHideModal(){
-        const modal = document.querySelectorAll(".confirm-modal")[this.props.id];
-        modal.style.display = "none";
-        const card = document.querySelectorAll(".card-content")[this.props.id];
-        card.style.display = "flex";
-    }
+	function _handleConfirm() {
+		_handleHideModal();
+		handleDelete(id);
+	}
 
-    _handleConfirm(){
-        this._handleHideModal();
-        this.props.handleDelete(this.props.id);
-    }
-
-    render(){
-        return(
-            <section className="confirm-modal" data-id={this.props.id}>
-                <h3 className="title-confirm-modal">Atenção!</h3>
-                <p className="description-confirm-modal">Você tem certeza que deseja excluir esta tarefa?</p>
-                <section className="buttons-confirm-modal">
-                    <button className="cancel-button-modal" onClick={this._handleHideModal.bind(this)}>Não</button>
-                    <button className="confirm-button-modal" onClick={this._handleConfirm.bind(this)}>Sim</button>
-                </section>
-            </section>
-        );
-    }
+	return (
+		<section className="confirm-modal" data-id={id}>
+			<h3 className="title-confirm-modal">Atenção!</h3>
+			<p className="description-confirm-modal">
+				Você tem certeza que deseja excluir esta tarefa?
+			</p>
+			<section className="buttons-confirm-modal">
+				<button className="cancel-button-modal" onClick={_handleHideModal}>
+					Não
+				</button>
+				<button className="confirm-button-modal" onClick={_handleConfirm}>
+					Sim
+				</button>
+			</section>
+		</section>
+	);
 }
-
 
 export default ConfirmModal;
--- src/components/registerForm/registerForm.jsx
+++ src/components/registerForm/registerForm.jsx
@@ -1,58 +1,47 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "../../assets/registerForm/style.css";
 
-class RegisterForm extends Component {
-	constructor(props) {
-		super(props);
-		this.title = "";
-		this.description = "";
-	}
+function RegisterForm({ handleSubmit }) {
+	const [title, setTitle] = useState("");
+	const [description, setDescription] = useState("");
 
-	_handleChangeTitle(event) {
+	function _handleChangeTitle(event) {
 		event.stopPropagation();
-		this.title = event.target.value;
+		setTitle(event.target.value);
 	}
 
-	_handleChangeDescription(event) {
+	function _handleChangeDescription(event) {
 		event.stopPropagation();
-		this.description = event.target.value;
+		setDescription(event.target.value);
 	}
 
-	_handleSubmit(event) {
+	function _handleSubmit(event) {
 		event.preventDefault();
 		event.stopPropagation();
 
-		this.props.handleSubmit(this.title, this.description);
+		handleSubmit(title, description);
 	}
 
-	render() {
-		return (
-			<section>
-				<form
-					className="form-register-form"
-					onSubmit={this._handleSubmit.bind(this)}
-				>
-					<section className="form-register-titulo">
-						<label>Título</label>
-						<input
-							maxLength="72"
-							onChange={this._handleChangeTitle.bind(this)}
-						></input>
-					</section>
-					<section className="form-register-descricao">
-						<label>Descrição</label>
-						<textarea
-							rows="5"
-							wrap="hard"
-							maxLength="109"
-							onChange={this._handleChangeDescription.bind(this)}
-						></textarea>
-					</section>
-					<button className="form-register-button">Salvar Tarefa</button>
-				</form>
-			</section>
-		);
-	}
+	return (
+		<section>
+			<form className="form-register-form" onSubmit={_handleSubmit.bind(this)}>
+				<section className="form-register-titulo">
+					<label>Título</label>
+					<input maxLength="72" onChange={_handleChangeTitle}></input>
+				</section>
+				<section className="form-register-descricao">
+					<label>Descrição</label>
+					<textarea
+						rows="5"
+						wrap="hard"
+						maxLength="109"
+						onChange={_handleChangeDescription}
+					></textarea>
+				</section>
+				<button className="form-register-button">Salvar Tarefa</button>
+			</form>
+		</section>
+	);
 }
 
 export default RegisterForm;
